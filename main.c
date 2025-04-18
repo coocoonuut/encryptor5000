67df1e5844dca8bcc4e4fe5a95ad6e591a8e63e5
coocoonuut <goddmartee@gmail.com>
2025-04-18 16:54:16 -0600

    Initial Commit ❤️

---
+++ main.c
@@ -0,0 +1,110 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+# define NUMBER_LETTERS 26
+# define NULL_CHARACTER '\0'
+# define TEXT_LENGTH 101
+
+char* caesar(const char* input, int key);
+char* encrypt(const char* input, int key);
+char* decrypt(const char* input, int key);
+void decrypting(const char* input);
+int randomInt(int min, int max);
+
+int main() {
+    srand(time(NULL));
+
+    char text[TEXT_LENGTH];
+    do {
+        printf("Enter text (Minimum 100 characters): ");
+        fgets(text, TEXT_LENGTH, stdin);
+
+        // Si el texto ingresado es muy largo, fgets NO captura el '\n'
+        // y el búfer de entrada todavía tiene caracteres pendientes.
+        if (strchr(text, '\n') == NULL) {
+            // Limpiar el búfer de entrada
+            int ch;
+            while ((ch = getchar()) != '\n' && ch != EOF) {}
+
+            printf("Texto demasiado largo. Intentalo de nuevo.\n");
+        } else {
+            // Se quitó el \n, solo si estaba
+            text[strcspn(text, "\n")] = '\0';
+            break;  // Entrada válida
+        }
+    } while (1);
+
+    int key = randomInt(1, NUMBER_LETTERS - 1);
+
+    char* encrypted = encrypt(text, key);
+
+    if (encrypted != NULL) {
+        printf("Texto cifrado: %s\n", encrypted);
+
+        free(encrypted);
+    } else {
+        printf("Error al cifrar el texto.\n");
+    }
+
+    // decrypting("Elix Jrkal!");
+
+    return 0;
+}
+
+char* caesar(const char* input, int key) {
+    int len = strlen(input);
+    char* output = (char*)malloc((len + 1) * sizeof(char));
+
+    if (output == NULL) {
+        return NULL;
+    }
+
+    int i = 0;
+    while (input[i] != NULL_CHARACTER) {
+        char ch = input[i];
+
+        /*
+            Durante el tiempo de compilacion, el compilador
+            procesa (Type Promotion) los caracteres a
+            numeros enteros (Codigo ASCII).
+
+            ej. 'a' es igual a 97.
+        */
+        if (isalpha(ch)) {
+            char base = isupper(ch) ? 'A' : 'a';
+
+            output[i] = (ch - base + key + NUMBER_LETTERS) % NUMBER_LETTERS + base;
+        } else {
+            output[i] = ch;
+        }
+
+        i++;
+    }
+
+    output[len] = NULL_CHARACTER;
+    return output;
+}
+
+char* encrypt(const char* input, int key) {
+    return caesar(input, key);
+}
+
+char* decrypt(const char* input, int key) {
+    return caesar(input, -key);
+}
+
+void decrypting(const char* input) {
+    for (int key = 1; key < NUMBER_LETTERS; key++) {
+        char* decrypted = decrypt(input, key);
+
+        printf("Key %d: %s\n", key, decrypted);
+    }
+}
+
+int randomInt(int min, int max) {
+    if (max < min) return 0;
+    return rand() % (max - min + 1) + min;
+}
