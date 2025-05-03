9f5d292d6eec8378328335a3e239a1fb51568cf7
coocoonuut <goddmartee@gmail.com>
2025-05-03 16:39:23 -0600

    Project: Vigenere function added

---
+++ src/crypto/vigenere.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <string.h>
+
+#include "crypto/vigenere.h"
+
+void vigenere(const char* plaintext, char* result, const char* key, int isEncrypted) {
+    int keyLen = (int)strlen(key);
+
+    for (int i = 0, j = 0; plaintext[i] != NULL_CHARACTER; i++) {
+        char c = plaintext[i];
+
+        if (isalpha(c)) {
+            int keyShift = toupper(key[j % keyLen]) - 'A';
+
+            if (isEncrypted) {
+                result[i] = (char)((c - 'A' - keyShift + ALPHABET_SIZE) % ALPHABET_SIZE + 'A');
+            } else {
+                result[i] = (char)((c - 'A' + keyShift) % ALPHABET_SIZE + 'A');
+            }
+
+            j++;
+        } else {
+            result[i] = c;
+        }
+    }
+
+    result[strlen(plaintext)] = NULL_CHARACTER;
+}
