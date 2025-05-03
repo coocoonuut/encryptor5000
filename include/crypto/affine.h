d4dd882e46eeb3abb9d3e3b4131a819f10c11072
coocoonuut <goddmartee@gmail.com>
2025-05-03 16:22:35 -0600

    Project: Affine header function added

---
+++ include/crypto/affine.h
@@ -0,0 +1,15 @@
+#ifndef AFFINE_H
+#define AFFINE_H
+
+/**
+* Cifra o descifra un texto usando el cifrado Affín
+ * @param plaintext Texto de entrada
+ * @param result Un String Pointer que devuelve el resultado del Cifrado/Descifrado
+ * @param comprime26 Un numero coprimo de 26
+ * @param displacement Es el desplazamiento
+ * @param isEncrypted 1 para descifrar, 0 para cifrar
+ * @return Int devuelve 1 si hubo error o 0 se completo el cifrado/descifrado correctamente
+ */
+int affine(const char* plaintext, char* result, int comprime26, int displacement, int isEncrypted);
+
+#endif //AFFINE_H
