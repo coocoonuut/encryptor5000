e7d4aea057416e500b8adabd3bdb88c75bfd5447
coocoonuut <goddmartee@gmail.com>
2025-05-03 16:40:17 -0600

    Project: Constants header added

---
+++ include/constants.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define ALPHABET_SIZE 26
+#define NULL_CHARACTER '\0'
+#define ENTER_CHARACTER '\n'
+#define MAX_KEY_LENGTH 20
+#define MAX_LEN 5000
+#define INPUT_BUFFER_SIZE 10
+#define NUM_COPRIMES 12
+#define SPANISH_IO 0.0755
+#define MSG_ERROR_INPUT "<ERROR DE LECTURA>\n"
+#define MSG_INVALID_OPTION "<ERROR> Por favor, elige las opciones disponibles.\n"
+#define MSG_INVALID_NUMBER "<ERROR> Entrada invalida. Debe ingresar un numero.\n\n"
+#define MSG_ENCRYPT "Escriba un texto que quiera cifrar:\n"
+#define MSG_DECRYPT "Escriba un texto que quiera descifrar:\n"
+
+#endif
