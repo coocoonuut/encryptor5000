83d6d6a1d8ce8a810663267e1bd504edf38db418
coocoonuut <goddmartee@gmail.com>
2025-05-03 16:41:00 -0600

    Project: Globals Variables header added

---
+++ include/globals.h
@@ -0,0 +1,8 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern const double spanishFrequencies[];
+extern const double englishFrequencies[];
+extern const int comprimeNumbers[];
+
+#endif //GLOBALS_H
