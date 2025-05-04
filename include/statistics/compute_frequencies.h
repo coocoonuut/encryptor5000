026c4d69e35f87ab82a9ac197de16284fe67c93b
coocoonuut <goddmartee@gmail.com>
2025-05-03 20:38:56 -0600

    Project: compute_frequencies header defined

---
+++ include/statistics/compute_frequencies.h
@@ -0,0 +1,6 @@
+#ifndef COMPUTE_FREQUENCIES_H
+#define COMPUTE_FREQUENCIES_H
+
+void computeFrequencies(const char* text, unsigned long length, int* freqs, int* total);
+
+#endif //COMPUTE_FREQUENCIES_H
