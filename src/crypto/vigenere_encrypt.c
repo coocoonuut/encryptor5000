c8ffef6ce4539bac8484cd28e2d9a384f1e67f01
coocoonuut <goddmartee@gmail.com>
2025-05-31 22:04:18 -0600

    refactor(crypto): kiss

--- src/crypto/vigenere_encrypt.c
+++ src/crypto/vigenere_encrypt.c
@@ -5,14 +5,11 @@
 #include "constants.h"
 
 void vigenere_encrypt(const char* text, char* encrypted_text, char* key) {
-    int key_length = (int)strlen(key);
-
     for (int i = 0, j = 0; text[i] != NULL_CHARACTER; i++) {
         char c = text[i];
 
         if (isalpha(c)) {
-            encrypted_text[i] = (char)((c - 'A' - (key[j % key_length] - 'A') + ALPHABET_SIZE) % ALPHABET_SIZE + 'A');
-
+            encrypted_text[i] = (char)(c - 'A' - (key[j % strlen(key)] - 'A') % ALPHABET_SIZE + 'A');
             j++;
         } else {
             encrypted_text[i] = c;
