b97f9895433c65b90400108c898337528c3af762
coocoonuut <goddmartee@gmail.com>
2025-06-01 19:01:28 -0600

    feat(crypto): add affine_encrypt function

--- include/crypto/affine_encrypt.h
+++ include/crypto/affine_encrypt.h
@@ -1,6 +1,6 @@
 #ifndef AFFINE_ENCRYPT_H
 #define AFFINE_ENCRYPT_H
 
-void affine_encrypt(char* text, int coprimeOf26, int displacement, char* encrypted_text);
+void affine_encrypt(const char* text, int coprimeOf26, int displacement, char* encrypted_text);
 
 #endif //AFFINE_ENCRYPT_H
---
+++ src/crypto/affine_encrypt.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <string.h>
+
+#include "crypto/affine_encrypt.h"
+#include "constants.h"
+
+void affine_encrypt(const char* text, int coprimeOf26, int displacement, char* encrypted_text) {
+    for (int i = 0; text[i] != NULL_CHARACTER; i++) {
+        char c = text[i];
+
+        if (isalpha(c)) {
+            encrypted_text[i] = (char)((coprimeOf26 * (c - 'A') + displacement) % ALPHABET_SIZE + 'A');
+        } else {
+            encrypted_text[i] = c;
+        }
+    }
+
+    encrypted_text[strlen(encrypted_text)] = NULL_CHARACTER;
+}
